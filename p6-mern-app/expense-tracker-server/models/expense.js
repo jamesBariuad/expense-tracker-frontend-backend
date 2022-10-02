6e64c65c9a27f25abc5ab64df3dca5caa0d2ab8e
james bariuad <jamesbariuad@gmail.com>
2022-10-02 17:18:00 +0800

    fix:  schema can now return collection data

--- p6-mern-app/expense-tracker-server/models/expense.js
+++ p6-mern-app/expense-tracker-server/models/expense.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-// const Int32 = require("mongoose-int32");
 
 const expenseSchema = new Schema({
   description: String,
   category: String,
-  value: Int32,
-  date: String,
+  value: Number,
+  date: String
 });
 
-module.exports = mongoose.model("Expense", expenseSchema);
+module.exports = mongoose.model("Expense", expenseSchema, "expense");
